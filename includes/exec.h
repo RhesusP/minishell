cf19dab44f1d53d9fd671396481a6e7a556ce406
tbarde-c <thbc69@gmail.com>
2023-02-28 14:06:57 +0100

    exec.h --> Updated prototypes

--- includes/exec.h
+++ includes/exec.h
@@ -6,12 +6,23 @@
 /*   By: tbarde-c <tbarde-c@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 12:51:50 by tbarde-c          #+#    #+#             */
-/*   Updated: 2023/02/28 12:52:25 by tbarde-c         ###   ########.fr       */
+/*   Updated: 2023/02/28 14:05:25 by tbarde-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_H
 # define EXEC_H
+
+/*
+**	Exec utils
+*/
+char	*ft_strjoin_spaced(char const *s1, char const *s2);
+
+
+/*
+**	Execution functions
+*/
+char	*get_full_cmd(t_word *parse);
 
 
 #endif
