4a091ff3cc4e5b2e1c994f32e548c084e99f2527
tbarde-c <thbc69@gmail.com>
2023-02-28 15:41:25 +0100

    environment.c + exec.h --> Added environment.c and updated prototypes

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: tbarde-c <tbarde-c@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 12:51:50 by tbarde-c          #+#    #+#             */
-/*   Updated: 2023/02/28 14:44:38 by tbarde-c         ###   ########.fr       */
+/*   Updated: 2023/02/28 15:25:21 by tbarde-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@
 /*
 **	Exec utils
 */
-int		get_arg_len(t_word *parse);
-void	free_all(char **str);
+int			get_arg_len(t_word *parse);
+void		free_all(char **str);
 
 /*
 **	Execution functions
 */
-char	**get_full_cmd(t_word *parse);
+char		**get_full_cmd(t_word *parse);
+
+/*
+**	Environment functions
+*/
+t_env_var	*get_env_custom(char *key_to_find, t_env_var *env);
 
 
 #endif
---
+++ srcs/environment.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   environment.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbarde-c <tbarde-c@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 15:06:04 by tbarde-c          #+#    #+#             */
+/*   Updated: 2023/02/28 15:41:05 by tbarde-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+#include "../includes/exec.h"
+
+
+/*
+**	Get the pointer to the correct variable we need
+**	in the minishell environment
+*/
+t_env_var	*get_env_custom(char *key_to_find, t_env_var *env)
+{
+	int			flag;
+	t_env_var	*cpy;
+
+	cpy = env;
+	flag = 0;
+	while (cpy && flag == 0)
+	{
+		if (key_to_find == cpy->key)
+		{
+			flag = 1;
+			break ;
+		}
+		cpy = cpy->next;
+	}
+	if (flag == 1)
+		return (cpy);
+	return (NULL);
+}
+
+/*
+**	Main test -- WARNING : ERASE IT
+*/
+int	main()
+{
+	t_env_var	env;
+	t_env_var	env2;
+	t_env_var	env3;
+	t_env_var	env4;
+	t_env_var	*res;
+
+	env.key = "dza";
+	env.next = &env2;
+	env2.key = "??";
+	env2.next = &env3;
+	env3.key = "PATH";
+	env3.values = malloc(sizeof(char *));
+	env3.values[0] = "Numero 0";
+	env3.next = &env4;
+	env4.key = "Nope";
+	env4.next = NULL;
+	res = get_env_custom("PATH", &env);
+	printf ("%s\n", res->values[0]);
+	free(env3.values);
+	return 0;
+}
