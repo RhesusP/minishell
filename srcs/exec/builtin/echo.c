edf1d4822add5c6e7e8bede26fcc28528c0801a0
tbarde-c <thbc69@gmail.com>
2023-03-01 13:15:57 +0100

    echo.c --> Created builtin echo option -n doesn't work yet

---
+++ srcs/exec/builtin/echo.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbarde-c <tbarde-c@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 12:51:14 by tbarde-c          #+#    #+#             */
+/*   Updated: 2023/03/01 13:14:08 by tbarde-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/minishell.h"
+#include "../../../includes/exec.h"
+
+/*
+**	Echo with no option
+*/
+static void	exec_echo_no_option(char **cmd)
+{
+	int	i;
+
+	i = 1;
+	while (cmd[i])
+	{
+		printf("%s", cmd[i]);
+		if (cmd[i + 1])
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+}
+
+/*
+**	Echo -n
+*/
+static void	exec_echo_dash_n(char **cmd)
+{
+	int	i;
+
+	i = 3;
+	if (!cmd[2])
+		return ;
+	else
+		while (cmd[i])
+		{
+			printf("%s", cmd[i]);
+			if (cmd[i + 1])
+				printf(" ");
+			i++;
+		}
+}
+
+/*
+**	Display the arg
+**	-n : no newline after display
+**	WARNING : DOESN'T WORK YET
+*/
+void	exec_echo(char **cmd)
+{
+	int	i;
+
+	i = 0;
+	if (!cmd[1])
+		printf("\n");
+	else if (cmd[1] == "-n")
+		exec_echo_dash_n(cmd);
+	else
+		exec_echo_no_option(cmd);
+}
+
+/*
+**	Main test	WARNING : TO ERASE
+*/
+int main()
+{
+	char **cmd;
+	cmd = malloc(sizeof(char *) * 4);
+	cmd[0] = "echo";
+	cmd[1] = "coucou";
+	cmd[2] = "coucou";
+	cmd[3] = NULL;
+	exec_echo(cmd);
+	free(cmd);
+	return 0;
+}
