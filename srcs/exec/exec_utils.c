4360ce2d820883f104264e11d60f9acc59f890fd
tbarde-c <thbc69@gmail.com>
2023-03-01 12:49:41 +0100

    exec_utils.c --> Added is_builtin function (to complete)

--- srcs/exec/exec_utils.c
+++ srcs/exec/exec_utils.c
@@ -6,12 +6,33 @@
 /*   By: tbarde-c <tbarde-c@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:37:14 by tbarde-c          #+#    #+#             */
-/*   Updated: 2023/02/28 15:44:14 by tbarde-c         ###   ########.fr       */
+/*   Updated: 2023/03/01 12:49:24 by tbarde-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include "../includes/exec.h"
+
+/*
+**	Check if the command is a builtin
+**	and execute it if it is indeed a builtin
+*/
+int	is_builtin(char *cmd)
+{
+	if (cmd == "exit")
+		//return(exec_exit(), SUCCESS)
+	if (cmd == "export")
+		//return(exec_export(), SUCCESS)
+	if (cmd == "pwd")
+		//return(exec_pwd(), SUCCESS)
+	if (cmd == "unset")
+		//return(exec_unset(), SUCCESS)
+	if (cmd == "echo")
+		//return(exec_echo(), SUCCESS)
+	if (cmd == "env")
+		//return(exec_env(), SUCCESS)
+	return (FAILURE);
+}
 
 /*
 **	Get the number of args for the execution of one programm
