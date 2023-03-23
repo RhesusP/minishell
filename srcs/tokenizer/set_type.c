c756d6ee308b6bf0c0ce06224d47e8fc2bb4231d
Thibault <tbarde-c@student.42lyon.fr>
2023-03-23 14:09:11 +0100

    set_type.c --> Corrected typo, now when we enter a global variable that already existed, the global variable is actualized, and no double is added to the global variable structure

--- srcs/tokenizer/set_type.c
+++ srcs/tokenizer/set_type.c
@@ -83,8 +83,7 @@ void	give_cmd_type(t_word **lst, t_env_var **globals)
 				{
 					//printf("\033[32mwe can execute this assignation\033[39m\n");
 					//delete_word(word, lst);
-					printf("RESUUUULT === %d\n", actualize_global_var(globals, word->word) == FAILURE);
-					if (actualize_global_var(globals, word->word) == FAILURE);
+					if (actualize_global_var(globals, word->word) == FAILURE)
 					{
 						printf("ADDBACK\n");
 						add_back_env_var(globals, create_env_var(word->word));
