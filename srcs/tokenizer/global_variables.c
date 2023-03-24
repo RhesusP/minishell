463f70dfb3367d0e885c3440ef480835bbfc890a
Thibault <tbarde-c@student.42lyon.fr>
2023-03-24 12:55:39 +0100

    global_variables.c --> Added some new ideas to make a better jb at replacing dollars --> Should be better to handle '$VAR' and command where there are multiple $VAR$VAR$VAR

--- srcs/tokenizer/global_variables.c
+++ srcs/tokenizer/global_variables.c
@@ -1,6 +1,6 @@
 
 #include "./../../includes/minishell.h"
-
+/*
 static char	*cpy_dollar_value(char *word, int dollar_position)
 {
 	int		size;
@@ -24,6 +24,30 @@ static char	*cpy_dollar_value(char *word, int dollar_position)
 	return (res);
 }
 
+char	*cpy_dollar_value(char *word, int i)
+{
+	int		dollar_pos;
+	int		malloc_size;
+	char	*res;
+
+	malloc_size = 0;
+	dollar_pos = i;
+	while(word[i] || word[i] != '\"')
+	{
+		malloc_size++;
+		i++
+	}
+	i = 0;
+	res = calloc(malloc_size + 1, sizeof(char));
+	while (word[dollar_pos] || word[dollar_pos] != '\"')
+	{
+		res[i] = word[dollar_pos];
+		i++;
+		dollar_pos++;
+	}
+	return (res);
+}
+
 static char	*get_dollar_key(char *word)
 {
 	int		i;
@@ -35,9 +59,13 @@ static char	*get_dollar_key(char *word)
 	if (word[i] == '\"')
 		return (NULL);
 	else if (word[i] == '$')
+	{
 		res = cpy_dollar_value(word, i);
+	}
 	return (res);
 }
+*/
+////////////////////////////////////////////////
 
 void	replace_dollar_sign(t_env_var **globals, t_word *word)
 {
@@ -59,11 +87,14 @@ void	replace_dollar_sign(t_env_var **globals, t_word *word)
 	}
 }
 
+/* OLD FUNCTION*/
 void	replace_global_variables(t_env_var **globals, t_word **words_lst)
 {
 	t_word	*current;
 
 	current = *words_lst;
+	//Il faudrait compter le nombre de $ total dans la phrase
+	//Il faut à chaque fois check si on est entre guillemets
 	while (current)
 	{
 		if (ft_strchr(current->word, '$'))
@@ -76,3 +107,118 @@ void	replace_global_variables(t_env_var **globals, t_word **words_lst)
 		current = current->next;
 	}
 }
+//////////////////////////////////////////////////////////////////
+/**
+ * @brief Count the number of dollar signs to know how many $VAR we have
+ * Doesn't count the $ if there are no char after it
+*/
+int	count_dollar_nbr(char *word)
+{
+	int	i;
+	int	dollar_nbr;
+
+	i = 0;
+	dollar_nbr = 0;
+	while (word[i])
+	{
+		if (word[i] == '$' && word[i + 1])
+			dollar_nbr++;
+		i++
+	}
+	return (dollar_nbr);
+}
+
+/**
+ * @brief Check if the $VAR is in between quotes ' ' or not
+ * @return FAILURE if no quotes // SUCCESS if quotes
+*/
+int	check_quotes(char *word, int dollar_pos)
+{
+	int	cpy;
+	int	flag;
+
+	cpy = dollar_pos;
+	flag = 0;
+	if (dollar_pos = 0)
+		return (FAILURE);
+	while (word[cpy])
+	{
+		if (word[cpy] == '\'')
+		{
+			flag = 1;
+			break ;
+		}
+		cpy--;
+	}
+	if (flag == 1)
+	{
+		while (word[dollar_pos])
+		{		
+			if (word[dollar_pos] == '\'')
+			{
+				flag = 2;
+				break ;
+			}
+			dollar_pos--;
+		}
+	}
+	if (flag == 2)
+		return (SUCCESS);
+	return (FAILURE);
+}
+
+/**
+ * @brief Replace the $VAR, but not if there is '$VAR '
+*/
+void	replace_dollar_sign(char *word, int dollar_pos)
+{
+	if (check_quotes(word, dollar_pos) == FAILURE)
+	{
+		// Copie / strdup de ce qu'on a avant le $ étudié
+		// replace_dollar_sign --> on arrête le remplacement quand on rencontre :
+		// 		- Fin du mot
+		// 		- Un guillemet simple ou double
+		// 		- Un autre signe $
+		// Copie de ce qu'on a aprèsle dollar étudié
+	}
+
+}
+
+/* NEW FUNCTION*/
+/**
+ * @brief Check each $VAR and replace it if necessary by its value
+*/
+void	replace_global_variables(t_env_var **globals, t_word **words_lst)
+{
+	t_word	*current;
+	int		dollar_nbr;
+	int		i;
+	int		j;
+
+
+	current = *words_lst;
+	while (current)
+	{
+		dollar_nbr = count_dollar_nbr(current->word);
+		j = 0;
+		i = 0;
+		while (i < dollar_nbr)
+		{
+			while (current->word[j] != '$')
+				j++;
+			replace_dollar_sign(current->word, j);
+		}
+	}
+	//Il faudrait compter le nombre de $ total dans la phrase
+	//Il faut à chaque fois check si on est entre guillemets
+
+}
+
+
+/*AUTRE IDEE --> remplacement au fur et à mesure
+	- On avance dans le mot
+	- A chaque fois qu'on rencontre un $ on stoppe, on regarde dans la strucutre par quoi on le remplace
+	- Si on rencontre un ' : on met un flag = 1 jusqu'à ce qu'on rencontr le prochain
+	- Quand flag = 1, on ne remplace pas les $
+	- Quand on rencontre le second ', on repasse le flag = 0
+	*/
