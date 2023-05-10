f023c7a6f3494d2f9965fe32fa354f0f2fe0ea64
Camille Bernot <camil.bernot.cb@gmail.com>
2023-05-10 20:43:12 +0200

    adding global variable expansion

--- srcs/expansion/var_expansion.c
+++ srcs/expansion/var_expansion.c
@@ -6,101 +6,11 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 08:37:25 by cbernot           #+#    #+#             */
-/*   Updated: 2023/05/10 18:23:06 by cbernot          ###   ########.fr       */
+/*   Updated: 2023/05/10 20:41:16 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../includes/minishell.h"
-
-int	is_quoted(char *str, char quote)
-{
-	int	i;
-	int	nb;
-
-	i = 0;
-	nb = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == quote)
-			nb++;
-		i++;
-	}
-	if (nb == 2)
-		return (1);
-	return (0);
-}
-
-// char	*remove_quotes(char *str, char quote)
-// {
-// 	int	len_before;
-// 	int	len_between;
-// 	char	*bef;
-// 	char	*bet;
-// 	char	*aft;
-// 	char 	*res;
-// 	char	*temp;
-
-// 	len_before = 0;
-// 	while (str[len_before] != '\0' && str[len_before] != quote)
-// 		len_before++;
-// 	bef = ft_strndup(str, len_before);
-// 	len_between = len_before + 1;
-// 	while (str[len_between] != '\0' && str[len_between] != quote)
-// 		len_between++;
-// 	bet = ft_strndup(&str[len_before + 1], len_between - 1 - len_before);
-// 	aft = ft_strdup(&str[len_between + 1]);
-// 	temp = ft_strjoin(bef, bet);
-// 	res = ft_strjoin(temp, aft);
-// 	printf("bef: %s\n", bef);
-// 	printf("bet: %s\n", bet);
-// 	printf("aft: %s\n", aft);
-// 	free(temp);
-// 	free(bef);
-// 	free(bet);
-// 	free(aft);
-// 	free(str);
-// 	return (res);
-// }
-
-/**
- * @brief 
- * 
- * @param words_lst 
- * @param global_vars 
- * @param env_vars 
- * @return 0 if not, 1 if " '' ", 2 if ' "" '
- */
-int	is_multiple_quoted(char *str)
-{
-	int	i;
-	int	is_single;
-	int	is_double;
-	int	first;
-
-	i = 0;
-	first = 0;
-	is_single = 0;
-	is_double = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == '\'')
-		{
-			if (!first)
-				first = 2;
-			is_single = 1;
-		}
-		if (str[i] == '"')
-		{
-			if (!first)
-				first = 1;
-			is_double = 1;
-		}
-		i++;
-	}
-	if (is_single != 0 && is_double != 0)
-		return (first);
-	return (0);
-}
 
 char	**get_key_name(char *str)
 {
@@ -158,13 +68,14 @@ char	*values_to_str(char **tab)
 	return (res);
 }
 
-char	*get_values(char *key, t_env_var **lst)
+char	*get_values(char *key, t_env_var **lst, t_env_var **global)
 {
 	t_env_var	*current;
 	char		**values;
 	int			stop;
 
 	stop = 0;
+	values = 0;
 	if (!*lst || !key)
 		return (0);
 	if (ft_strcmp(key, "$") == 0)
@@ -188,10 +99,26 @@ char	*get_values(char *key, t_env_var **lst)
 		}
 		current = current->next;
 	}
+	if (!values && *global)
+	{
+		stop = 0;
+		current = *global;
+		while (current && !stop)
+		{
+			if (ft_strcmp(current->key, key) == 0)
+			{
+				values = current->values;
+				stop = 1;
+			}
+			current = current->next;
+		}
+	}
+	if (!values)
+		return ("");
 	return (values_to_str(values));
 }
 
-char	*get_vars(char *str, t_env_var **env)
+char	*get_vars(char *str, t_env_var **env, t_env_var **global)
 {
 	char	**tab;
 	char	*res;
@@ -205,14 +132,14 @@ char	*get_vars(char *str, t_env_var **env)
 	printf("key: %s\n", tab[1]);
 	printf("after: %s\n", tab[2]);
 	if (tab[1])
-		res = ft_strjoin(tab[0], get_values(tab[1], env));
+		res = ft_strjoin(tab[0], get_values(tab[1], env, global));
 	else
 		res = ft_strjoin(tab[0], "");
 	while (tab[2])
 	{
 		tab = get_key_name(tab[2]);
 		if (tab[1])
-			temp = ft_strjoin(tab[0], get_values(tab[1], env));
+			temp = ft_strjoin(tab[0], get_values(tab[1], env, global));
 		else
 			temp = ft_strjoin(tab[0], "");
 		res = ft_strjoin(res, temp);
@@ -220,12 +147,6 @@ char	*get_vars(char *str, t_env_var **env)
 	}
 	printf("\033[31mget var return: %s\n\033[39m", res);
 	return (res);
-}
-
-
-char	*replace_var_by_value(char *str, t_env_var **global, t_env_var **env)
-{
-	return (get_vars(str, env));
 }
 
 /* ---------------------------------------------------------------------------------*/
@@ -386,7 +307,7 @@ char	*join_tab(char **tab, int size)
 	return (res);
 }
 
-char	*get_quoted(char *str, t_env_var **env)
+char	*get_quoted(char *str, t_env_var **env, t_env_var **global)
 {
 	int		size;
 	char	**tab;
@@ -400,7 +321,7 @@ char	*get_quoted(char *str, t_env_var **env)
 	{
 		printf("tab[%d]: %s\n", i, tab[i]);
 		if (tab[i][0] != '\'')
-			tab[i] = get_vars(tab[i], env);
+			tab[i] = get_vars(tab[i], env, global);
 		if (tab[i][0] == '\'' || tab[i][0] == '"')
 			tab[i] = remove_quotes(tab[i]);
 		printf("new tab[%d]: %s\n", i, tab[i]);
@@ -409,9 +330,9 @@ char	*get_quoted(char *str, t_env_var **env)
 	return (join_tab(tab, size));
 }
 
-char	*simplify_quotes(char *str, t_env_var **env)
+char	*simplify_quotes(char *str, t_env_var **env,t_env_var **global)
 {
-	return (get_quoted(str, env));
+	return (get_quoted(str, env, global));
 }
 
 t_word	**var_expansion(t_word **words_lst, t_env_var **global_vars, t_env_var **env_vars)
@@ -424,38 +345,9 @@ t_word	**var_expansion(t_word **words_lst, t_env_var **global_vars, t_env_var **
 	current = *words_lst;
 	while (current)
 	{
-		current->word = simplify_quotes(current->word, env_vars);
-		// quoted_case = is_multiple_quoted(current->word);
-		// if (quoted_case == 0)
-		// {
-		// 	if (is_quoted(current->word, '\''))
-		// 	{
-		// 		printf("\033[35m%s is single quoted\033[39m\n", current->word);
-		// 		current->word = remove_quotes(current->word, '\'');
-		// 	}
-		// 	else if (is_quoted(current->word, '"'))
-		// 	{
-		// 		printf("\033[35m%s is double quoted\033[39m\n", current->word);
-		// 		current->word = replace_var_by_value(current->word, global_vars, env_vars);
-		// 		current->word = remove_quotes(current->word, '"');
-		// 	}
-		// 	else
-		// 		current->word = replace_var_by_value(current->word, global_vars, env_vars);
-				
-		// }
-		// else
-		// {
-		// 	if (quoted_case == 1)							// " '' "
-		// 	{
-		// 		current->word = remove_quotes(current->word, '"');
-		// 		// Then need to handle ''
-		// 	}
-		// 	else if (quoted_case == 2)						// ' "" '
-		// 		printf("\033[35mCase '\"\"'033[39m\n");
-		// }
+		current->word = simplify_quotes(current->word, env_vars, global_vars);
 		current = current->next;
 	}
-
 
 	return (words_lst);
 }
